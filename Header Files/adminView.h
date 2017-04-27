ce6a21324f59391bcb1cbfe8c6d522d26703064d
Jonathan Martin <jonmartin721@gmail.com>
2017-04-27 14:23:58 -0500

    Almost done implementing password change for admins.

--- Header Files/adminView.h
+++ Header Files/adminView.h
@@ -14,6 +14,7 @@ public:
 
 	public slots:
 	void on_changePasswordButton_clicked();
+	void on_submitButton_clicked();
 
 	private slots:
 
--- Source Files/adminView.cpp
+++ Source Files/adminView.cpp
@@ -1,5 +1,7 @@
 #include "Header Files\stdafx.h"
 
+int userlocation_admin = 0;
+
 adminView::adminView(QWidget *parent)
 	: QMainWindow(parent)
 {
@@ -22,4 +24,68 @@ void adminView::on_changePasswordButton_clicked()
 
 	//display schedule
 
+}
+
+void adminView::on_submitButton_clicked()
+{
+	vector<users>allUsers = populateUsers();
+	vector<Student>allStudents = populateStudents();
+	vector<classes>allClasses = populateClasses();
+	vector<records>allRecords = populateRecords();
+	vector<faculty>allFaculty = populateFaculty();
+
+	QString newPassword;
+	QString currentPasswordTyped = ui.currentPasswordField->text();
+	QString currentPassword;
+	string username = allUsers[userlocation_admin].username;
+	string newVerifyPassword;
+	//getting user password
+	for (int i = 0; i < allUsers.size(); i++)
+	{
+		if (allStudents[userlocation_admin].userName == allUsers[i].username)
+		{
+			currentPassword = QString::fromStdString(allUsers[i].password);
+			break;
+		}
+	}
+
+	//getcurrentPassword
+	if (currentPasswordTyped == "")
+	{
+		ui.passwordChangeStatusLabel->setStyleSheet("QLabel { background-color : red; color : white; }");
+		ui.passwordChangeStatusLabel->setText("Current password can not be blank!");
+	}
+	else if (currentPasswordTyped != currentPassword)
+	{
+		ui.passwordChangeStatusLabel->setStyleSheet("QLabel { background-color : red; color : white; }");
+		ui.passwordChangeStatusLabel->setText("Incorrect current password!");
+	}
+	else
+	{
+		newPassword = ui.newPasswordField->text();
+		if (newPassword != ui.verifyNewPasswordField->text())
+		{
+			ui.passwordChangeStatusLabel->setStyleSheet("QLabel { background-color : red; color : white; }");
+			ui.passwordChangeStatusLabel->setText("The new password does not match!");
+		}
+		else if (newPassword == "")
+		{
+			ui.passwordChangeStatusLabel->setStyleSheet("QLabel { background-color : red; color : white; }");
+			ui.passwordChangeStatusLabel->setText("The new password can not be blank!");
+		}
+		else
+		{
+			ui.currentPasswordField->setText("");
+			ui.newPasswordField->setText("");
+			ui.verifyNewPasswordField->setText("");
+
+			newVerifyPassword = newPassword.toStdString();
+			changePassword(username, newVerifyPassword);
+
+			ui.passwordChangeStatusLabel->setStyleSheet("QLabel { background-color : green; color : white; }");
+			ui.passwordChangeStatusLabel->setText("Password has been changed!");
+		}
+
+
+	}
 }

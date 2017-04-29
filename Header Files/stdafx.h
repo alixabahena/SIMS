87593fbfab8b70b7e0f3247a5286f5777f2a7dbf
AlixaBahena <alixabahena@gmail.com>
2017-04-29 09:24:50 -0500

    Found a better way to display using SQLQueryModel

--- Header Files/stdafx.h
+++ Header Files/stdafx.h
@@ -36,6 +36,9 @@
 #include <QDesktopWidget>
 #include <qdialog.h>
 #include <string.h>
+#include <qsqltablemodel.h>
+#include <qsqlquery.h>
+#include <qsqlquerymodel.h>
 
 //normal libraries
 #include <string>
